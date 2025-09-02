c890d65569a5e204796f1008f14897592c0340fd
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-09-02 15:48:33 +0200

    Add pagination functionality to List

--- cocktails/src/components/List/index.tsx
+++ cocktails/src/components/List/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import type { ListItem as ListItemType } from '../../utilities/types';
 import ListItem from '../ListItem';
 import styles from './style.module.css';
@@ -7,16 +8,37 @@ type ListProps = {
 };
 
 export default function List({ items }: ListProps) {
+	const [page, setPage] = useState(0);
+
+	const pageSize = 10;
+	const pageCount = Math.ceil(items.length / pageSize);
+	const start = page * pageSize;
+	const end = start + pageSize;
+
+	const handleOnPrevious = () => setPage((prev) => Math.max(prev - 1, 0));
+	const handleOnNext = () =>
+		setPage((prev) => Math.min(prev + 1, pageCount - 1));
+
 	return (
-		<ul className={styles.list}>
-			{items.map((item) => (
-				<ListItem
-					key={item.id}
-					id={item.id}
-					label={item.label}
-					link={item.link}
-				/>
-			))}
-		</ul>
+		<section>
+			<ul className={styles.list}>
+				{items
+					.map((item) => (
+						<ListItem
+							key={item.id}
+							id={item.id}
+							label={item.label}
+							link={item.link}
+						/>
+					))
+					.slice(start, end)}
+			</ul>
+			{pageCount > 1 && (
+				<>
+					<button onClick={handleOnPrevious}>Prev</button>
+					<button onClick={handleOnNext}>Next</button>
+				</>
+			)}
+		</section>
 	);
 }
