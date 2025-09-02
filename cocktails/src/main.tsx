c73ac89fdbd72e78114dba2358c997815b0502c5
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-09-02 11:44:12 +0200

    Add SearchView with route

--- cocktails/src/main.tsx
+++ cocktails/src/main.tsx
@@ -4,6 +4,7 @@ import './styles/main.css';
 import HomeView from './views/HomeView';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import LayoutView from './views/LayoutView';
+import SearchView from './views/SearchView';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
 			<Routes>
 				<Route element={<LayoutView />}>
 					<Route index element={<HomeView />} />
+					<Route path="/search" element={<SearchView />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
---
+++ cocktails/src/views/SearchView/index.tsx
@@ -0,0 +1,5 @@
+import styles from './style.module.css';
+
+export default function SearchView() {
+	return <div className={styles.searchview}>Hello SearchView</div>;
+}
