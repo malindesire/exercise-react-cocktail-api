6a0df8d30d44fc5726465b45a6bbaeb35d145bdb
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-09-02 12:30:39 +0200

    Render search results in list

--- cocktails/src/views/SearchView/index.tsx
+++ cocktails/src/views/SearchView/index.tsx
@@ -1,5 +1,35 @@
+import { useEffect, useState } from 'react';
+import List from '../../components/List';
 import styles from './style.module.css';
+import { searchCocktailByName } from '../../utilities/api';
+import { mapRawCocktailData } from '../../utilities/mapRawCocktailData';
+import type { Cocktail } from '../../utilities/types';
+import type { ListItem } from '../../utilities/types';
 
 export default function SearchView() {
-	return <div className={styles.searchview}>Hello SearchView</div>;
+	const [result, setResult] = useState<Cocktail[]>([]);
+
+	useEffect(() => {
+		searchCocktailByName('margarita').then((data) => {
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			const mappedData = data.drinks.map((drink: any) =>
+				mapRawCocktailData(drink)
+			);
+			setResult(mappedData);
+		});
+	}, []);
+
+	const cocktails: ListItem[] = result.map((item) => {
+		return {
+			id: item.id,
+			label: item.name,
+			link: '',
+		};
+	});
+
+	return (
+		<div className={styles.searchview}>
+			<List items={cocktails} />
+		</div>
+	);
 }
