d4266b261ce7ed015be56e7294f9e9cdee664dfe
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-09-02 11:33:23 +0200

    Implement routes in main.tsx

--- cocktails/src/main.tsx
+++ cocktails/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/main.css';
-import Home from './components/Home';
+import Home from './views/Home';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import Layout from './views/Layout';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<Home />
+		<BrowserRouter>
+			<Routes>
+				<Route element={<Layout />}>
+					<Route index element={<Home />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
 	</StrictMode>
 );
