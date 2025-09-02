eb211928155a3d06e481076a80757da40d915691
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-09-02 12:00:17 +0200

    Add List component

---
+++ cocktails/src/components/List/index.tsx
@@ -0,0 +1,17 @@
+import type { ListItem as ListItemType } from '../../utilities/types';
+import ListItem from '../ListItem';
+import styles from './style.module.css';
+
+type ListProps = {
+	items: ListItemType[];
+};
+
+export default function List({ items }: ListProps) {
+	return (
+		<ul className={styles.list}>
+			{items.map((item) => (
+				<ListItem label={item.label} link={item.link} />
+			))}
+		</ul>
+	);
+}
