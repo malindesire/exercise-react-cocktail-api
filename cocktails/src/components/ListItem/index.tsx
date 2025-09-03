a885e599b3555a33f0abe3ba769135119c4edf45
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-09-03 22:46:14 +0200

    Change anchor element to NavLink

--- cocktails/src/components/ListItem/index.tsx
+++ cocktails/src/components/ListItem/index.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router';
 import type { ListItem as ListItemType } from '../../utilities/types';
 import styles from './style.module.css';
 
@@ -6,7 +7,7 @@ type ListItemProps = ListItemType;
 export default function ListItem({ label, link }: ListItemProps) {
 	return (
 		<li className={styles.listitem}>
-			<a href={link}>{label}</a>
+			<NavLink to={link}>{label}</NavLink>
 		</li>
 	);
 }
