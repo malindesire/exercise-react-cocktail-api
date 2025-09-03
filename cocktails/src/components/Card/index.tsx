6f32d0934c9bf8946d81d890c5a594869522e584
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-09-03 15:55:20 +0200

    Adjust styles for HomeView and Card

--- cocktails/src/components/Card/index.tsx
+++ cocktails/src/components/Card/index.tsx
@@ -1,7 +1,7 @@
-import clsx from 'clsx';
 import type { Image as ImageType } from '../../utilities/types';
 import Image from '../Image';
 import styles from './style.module.css';
+import { NavLink } from 'react-router';
 
 type CardProps = {
 	image: ImageType;
@@ -12,12 +12,12 @@ type CardProps = {
 
 export default function Card({ image, title, link, className }: CardProps) {
 	return (
-		<div className={clsx(styles.card, className)}>
-			<Image src={image.src} alt={image.alt} />
-			<p className={styles.title}>{title}</p>
-			<a className={styles.link} href={link}>
-				Read more...
-			</a>
+		<div className={className}>
+			<Image src={image.src} alt={image.alt} className={styles.image} />
+			<h2 className={styles.title}>{title}</h2>
+			<NavLink className={styles.link} to={link}>
+				Read more
+			</NavLink>
 		</div>
 	);
 }
--- cocktails/src/components/Image/index.tsx
+++ cocktails/src/components/Image/index.tsx
@@ -1,11 +1,14 @@
+import clsx from 'clsx';
 import type { Image as ImageType } from '../../utilities/types';
 import styles from './style.module.css';
 
-type ImageProps = ImageType;
+type ImageProps = ImageType & {
+	className?: string;
+};
 
-export default function Image({ src, alt }: ImageProps) {
+export default function Image({ src, alt, className }: ImageProps) {
 	return (
-		<div className={styles.image}>
+		<div className={clsx(styles.image, className)}>
 			<img src={src} alt={alt} />
 		</div>
 	);
