86c4c681e6175d89a44735b829cbd13fadd36786
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-09-03 09:56:50 +0200

    Add fetchCocktailById function

--- cocktails/src/utilities/api.ts
+++ cocktails/src/utilities/api.ts
@@ -6,6 +6,14 @@ export const fetchRandomCocktail = async () => {
 	return cocktail;
 };
 
+export const fetchCocktailById = async (id: string) => {
+	const res = await fetch(
+		`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+	);
+	const cocktail = await res.json();
+	return cocktail;
+};
+
 export const searchCocktailByName = async (name: string) => {
 	const res = await fetch(
 		`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`
