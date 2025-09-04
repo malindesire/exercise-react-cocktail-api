cfe92be843c852bb76a78381bdfd7ceaf32b8ce1
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-09-04 09:49:18 +0200

    Use data-mode routing

--- cocktails/src/main.tsx
+++ cocktails/src/main.tsx
@@ -1,22 +1,26 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/main.css';
-import HomeView from './views/HomeView';
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { createBrowserRouter, RouterProvider } from 'react-router';
 import LayoutView from './views/LayoutView';
+import HomeView from './views/HomeView';
 import SearchView from './views/SearchView';
 import CocktailView from './views/CocktailView';
 
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <LayoutView />,
+		children: [
+			{ index: true, element: <HomeView /> },
+			{ path: '/search', element: <SearchView /> },
+			{ path: '/cocktail/:id', element: <CocktailView /> },
+		],
+	},
+]);
+
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<BrowserRouter>
-			<Routes>
-				<Route element={<LayoutView />}>
-					<Route index element={<HomeView />} />
-					<Route path="/search" element={<SearchView />} />
-					<Route path="/cocktail/:id" element={<CocktailView />} />
-				</Route>
-			</Routes>
-		</BrowserRouter>
+		<RouterProvider router={router} />
 	</StrictMode>
 );
