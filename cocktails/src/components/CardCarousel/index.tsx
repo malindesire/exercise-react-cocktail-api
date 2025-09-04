1aacba956d5f83572ec3a0ad08a8f2b5e9324a7b
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-09-04 23:53:58 +0200

    Add animation to CardCarousel

--- cocktails/src/components/CardCarousel/index.tsx
+++ cocktails/src/components/CardCarousel/index.tsx
@@ -5,6 +5,7 @@ import { fetchRandomCocktail } from '../../utilities/api';
 import { mapRawCocktailData } from '../../utilities/mapRawCocktailData';
 import Card from '../Card';
 import Button from '../Button';
+import { AnimatePresence, motion } from 'framer-motion';
 
 type CardCarouselProps = {
 	initialItem: Cocktail;
@@ -19,13 +20,24 @@ export default function CardCarousel({ initialItem }: CardCarouselProps) {
 	};
 
 	return (
-		<section className={styles.cardcarousel}>
-			<Card
-				image={{ src: item.thumbnail, alt: item.name }}
-				title={item.name}
-				link={`/cocktail/${item.id}`}
-				className={styles.card}
-			/>
+		<section className={styles.cardCarousel}>
+			<AnimatePresence mode="wait">
+				<motion.div
+					key={item.id}
+					initial={{ x: '100%', opacity: 0 }} // startar utanför till höger
+					animate={{ x: '0%', opacity: 1 }} // landar i mitten
+					exit={{ x: '-100%', opacity: 0 }} // åker ut till vänster
+					transition={{ duration: 0.5, ease: 'easeInOut' }}
+					layout
+				>
+					<Card
+						image={{ src: item.thumbnail, alt: item.name }}
+						title={item.name}
+						link={`/cocktail/${item.id}`}
+						className={styles.card}
+					/>
+				</motion.div>
+			</AnimatePresence>
 			<Button label="Show new cocktail" onClick={() => fetchNewItem()} />
 		</section>
 	);
