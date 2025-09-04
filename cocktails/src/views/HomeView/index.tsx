539308127f3854c8451d4788dcc67da5619eada8
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-09-04 23:07:12 +0200

    Replace Card and Button with CardCarousel

--- cocktails/src/views/HomeView/index.tsx
+++ cocktails/src/views/HomeView/index.tsx
@@ -1,29 +1,17 @@
 import styles from './style.module.css';
 import { mapRawCocktailData } from '../../utilities/mapRawCocktailData';
 import type { Cocktail } from '../../utilities/types';
-import Card from '../../components/Card';
-import Button from '../../components/Button';
-import { useLoaderData, useRevalidator } from 'react-router';
+import { useLoaderData } from 'react-router';
+import CardCarousel from '../../components/CardCarousel';
 
 export default function HomeView() {
 	const data = useLoaderData();
 	const cocktail: Cocktail = mapRawCocktailData(data.drinks[0]);
-	const { revalidate } = useRevalidator();
 
 	if (!cocktail) return;
 	return (
 		<div className={styles.home}>
-			<Card
-				image={{ src: cocktail.thumbnail, alt: cocktail.name }}
-				title={cocktail.name}
-				link={`/cocktail/${cocktail.id}`}
-			/>
-			<Button
-				label="Show new cocktail"
-				onClick={() => {
-					revalidate();
-				}}
-			/>
+			<CardCarousel initialItem={cocktail} />
 		</div>
 	);
 }
