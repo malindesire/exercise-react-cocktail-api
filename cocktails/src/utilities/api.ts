3384bea0825c25e255fdbb6d8f0bf6a04dc3500c
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-09-10 10:13:39 +0200

    Add fetch functions for ingredients

--- cocktails/src/utilities/api.ts
+++ cocktails/src/utilities/api.ts
@@ -20,3 +20,17 @@ export const searchCocktailByName = async (name: string) => {
 	const data = await res.json();
 	return data.drinks;
 };
+
+export const fetchIngredients = async () => {
+	const res = await fetch(`${baseUrl}/list.php?i=list`);
+	if (!res.ok) throw new Error('Failed to fetch');
+	const data = await res.json();
+	return data.drinks;
+};
+
+export const fetchIngredient = async (name: string) => {
+	const res = await fetch(`${baseUrl}/search.php?i=${name}`);
+	if (!res.ok) throw new Error('Failed to fetch');
+	const data = await res.json();
+	return data.ingredients[0];
+};
