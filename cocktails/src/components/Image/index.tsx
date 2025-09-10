29324092c10b10920bca389972198a9d773439a7
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-09-10 15:37:25 +0200

    Don't show broken image and add background to container

--- cocktails/src/components/Image/index.tsx
+++ cocktails/src/components/Image/index.tsx
@@ -1,15 +1,22 @@
 import clsx from 'clsx';
 import type { Image as ImageType } from '../../utilities/types';
 import styles from './style.module.css';
+import { useState } from 'react';
 
 type ImageProps = ImageType & {
 	className?: string;
 };
 
 export default function Image({ src, alt, className }: ImageProps) {
+	const [error, setError] = useState(false);
 	return (
-		<div className={clsx(styles.image, className)}>
-			<img src={src} alt={alt} />
-		</div>
+		<figure className={clsx(styles.image, className)}>
+			<img
+				src={src}
+				alt={alt}
+				onError={() => setError(true)}
+				className={clsx(error && styles.error)}
+			/>
+		</figure>
 	);
 }
