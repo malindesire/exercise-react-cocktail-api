4d3e223a3f6f3cf0f6a0b9165bc2e0aece58451a
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-09-10 10:15:21 +0200

    Add loaders for ingredient data

--- cocktails/src/utilities/loaders.ts
+++ cocktails/src/utilities/loaders.ts
@@ -1,6 +1,8 @@
 import type { LoaderFunctionArgs } from 'react-router';
 import {
 	fetchCocktailById,
+	fetchIngredient,
+	fetchIngredients,
 	fetchRandomCocktail,
 	searchCocktailByName,
 } from './api';
@@ -22,4 +24,14 @@ export const searchCocktailsLoader = async ({
 	const q = url.searchParams.get('q');
 	if (!q) return [];
 	return await searchCocktailByName(q);
+};
+
+export const ingredientsLoader = async () => {
+	return await fetchIngredients();
+};
+
+export const ingredientLoader = async ({ params }: LoaderFunctionArgs) => {
+	const { name } = params;
+	if (!name) throw new Response('No ID provided', { status: 400 });
+	return await fetchIngredient(name);
 };
