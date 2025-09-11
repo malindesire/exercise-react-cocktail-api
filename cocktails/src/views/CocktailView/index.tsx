88bc2f841d8e024d9e5c0d4953840614476428f1
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-09-11 14:51:31 +0200

    Add link to ingredients page

--- cocktails/src/views/CocktailView/index.tsx
+++ cocktails/src/views/CocktailView/index.tsx
@@ -4,6 +4,7 @@ import { mapRawCocktailData } from '../../utilities/mapRawCocktailData';
 import type { Cocktail } from '../../utilities/types';
 import Image from '../../components/Image';
 import ItemInfo from '../../components/ItemInfo';
+import Clickable from '../../components/Clickable';
 
 export default function CocktailView() {
 	const data = useLoaderData();
@@ -12,11 +13,20 @@ export default function CocktailView() {
 	if (!cocktail) return;
 	return (
 		<main className={styles.cocktailView}>
-			<Image src={cocktail.thumbnail} alt={cocktail.name} />
+			<Image
+				src={cocktail.thumbnail}
+				alt={cocktail.name}
+				className={styles.image}
+			/>
 			<ItemInfo
 				item="cocktail"
 				cocktail={cocktail}
 				className={styles.info}
+			/>
+			<Clickable
+				element="a"
+				label="Learn more about ingredients"
+				link="/ingredients"
 			/>
 		</main>
 	);
